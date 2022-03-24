da2a5e7f8db4185d79a763cc46664aa66d78fdc9
FullFireZoo <yassine60000@gmail.com>
2022-03-24 17:36:41 +0100

    MaJ 2.1.0

--- Froont/corps/article/article.js
+++ Froont/corps/article/article.js
@@ -1,7 +1,31 @@
 const div = document.querySelector('.container')
 
 fetch("http://localhost:3000/Articles")
-  .then(response => response.text())
-  .then(article => bsd())
+  .then(response => response.json())
+  .then(article => {
+      
+    for(i of article){
+        console.log(i);
+    bsd(div, i.titre, i.contenu)}
 
+})
+    
+
+function bsd (selector, title,cont){
+    selector.innerHTML += 
+    `<div class="art1">
+    <ul>
+        <li>
+            <h2>${title}</h2>
+        </li>
+        <li>
+            <p>${cont}</p>
+        </li>
+    </ul>
+    <div class="btn">
+    <button>Modifier</button>
+    <button>Supprimer</button>
+    </div>
+</div>`
+}
 
